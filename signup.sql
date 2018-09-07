d041a12cd90879c0305db80463f37eb917424496
boss <r4b2c1@gmai.com>
2018-09-06 20:44:40 -0500

    update

--- signup.sql
+++ signup.sql
@@ -12,3 +12,31 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     console.log("connected ass id: "+connection.threadId);
     })
+
+    var sign = function(){
+        inquirer.propmpt([{
+
+            name:"name",
+            type: "input",
+            message: "what is your name?"
+        },{
+
+            name: "email",
+            type: "input",
+            message: "Your email please?"
+        },{
+
+            password: "password",
+            type: "input",
+            message: "Password needed."
+        }
+
+
+
+        }]).then(function(answer){
+            connection.query("Insert INTO input", {
+                name:answer.name,
+                
+            })
+        })
+    }
